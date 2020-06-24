846e086513559c8b430b13e2321492ae445eda4a
Agata Sumowska <agata.sumowska@gmail.com>
2020-06-24 21:47:52 +0200

    refactor

--- src/bob.clj
+++ src/bob.clj
@@ -2,33 +2,23 @@
   (:require [clojure.string :as str]))
 
 (defn last_character [s]
-  (if (empty? s)
-    ""
-    (str (nth s (dec (count s))))))
-
-(defn getAllAlphabeticCharsAsString [s]
-  (clojure.string/join
-    ""
-    (filter (fn [x] (Character/isLetter x)) s)))
+    (str/ends-with? (str/trimr s) "?"))
 
 (defn yell? [s]
-  (let [alphabeticCharsString (getAllAlphabeticCharsAsString s)]
-    (if (= alphabeticCharsString "") 
-      false
-      (= (str/upper-case alphabeticCharsString)
-      alphabeticCharsString))))
+  (some? (re-matches #"[^a-z]*[A-Z][^a-z]*" s)))
 
 (defn question? [s]
-  (let [last_char (last_character (str/trim s))]
-    (= last_char "?")))
+  (str/ends-with? (str/trimr s) "?"))
 
 (defn response-for [s] 
-  (cond 
-    (str/blank? s) "Fine. Be that way!" 
-    (and (yell? s ) (question? s) ) "Calm down, I know what I'm doing!"
-    (yell? s ) "Whoa, chill out!"
-    (question? s) "Sure."
-    :else "Whatever."
-  ))
+  ( let [isYelling (yell? s) isAsking (question? s)]
+    (cond 
+      (str/blank? s)            "Fine. Be that way!" 
+      (and isYelling isAsking)  "Calm down, I know what I'm doing!"
+      isYelling                 "Whoa, chill out!"
+      isAsking                  "Sure."
+      :else                     "Whatever."
+  )))
+  
 
 
